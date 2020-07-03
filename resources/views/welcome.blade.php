4e9ad75405cab43aeb0f4ef7518bb9f2eb35cc09
Alberto <alberto.germana@gmail.com>
2020-07-03 17:30:26 +0200

    View home -> progettone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Progettone
                 </div>
 
                 <div class="links">
