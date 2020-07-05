74d7d17cea618394c1db6d4bdac6391e2fa16115
Alberto <alberto.germana@gmail.com>
2020-07-05 13:32:46 +0200

    RF6 Completato
    Descrizione: Un proprietario che ha inserito uno o più appartamenti deve avere la possibilità di vedere i messaggi di richiesta ricevuti dagli utenti per gli appartamenti.
    In questa schermata dovrà vedere email e messaggio inserito dall’utente per ogni messaggio ricevuto.
    Non è prevista la possibilità di risposta da parte dell’utente UPRA (che risponderà direttamente via email fuori dalla piattaforma)
    Risultato:
    Eccezioni: /

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,30 +5,41 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+              <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <a href="{{route('createApartment')}}">Crea nuovo appartamento</a>
-
-
-                </div>
-                @if ($user_apartments)
-                  @foreach ($user_apartments as $apartment)
-
-                    <a href="{{route('showApartment',$apartment['id'])}}"><div class="card-header">{{$apartment -> title}}</div></a> 
-                    <div class="card-body">
-                      {{$apartment -> description}}
-
-                    </div>
-                  @endforeach
-                @endif
-
+              <div class="card-body">
+                  @if (session('status'))
+                      <div class="alert alert-success" role="alert">
+                          {{ session('status') }}
+                      </div>
+                  @endif
+                  <a href="{{route('createApartment')}}">Crea nuovo appartamento</a>
+              </div>
             </div>
+            @if ($user_apartments)
+              @foreach ($user_apartments as $apartment)
+                <div class="card">
+                  <a href="{{route('showApartment',$apartment['id'])}}"><div class="card-header">{{$apartment -> title}}</div></a>
+                  <div class="card-body">
+                    {{$apartment -> description}}
+                  </div>
+                </div>
+              @endforeach
+            @endif
+        </div>
+
+        <div class="col-md-4 flex-column-reverse d-flex">
+          @if ($user_messages)
+            @foreach ($user_messages[0] as $message)
+              <div class="card">
+                <div class="card-header">Appartamento: {{$message -> apartment -> title}}</div>
+                <div class="card-body">
+                  <b>Messaggio da: </b>{{$message -> email}} <br>
+                  <b>Testo:  </b>{{$message -> message}}
+                </div>
+              </div>
+            @endforeach
+          @endif
         </div>
     </div>
 </div>
